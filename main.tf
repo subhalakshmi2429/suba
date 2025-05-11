236fc4d0e0242c5d18e49ac1ec9171a2b475fc9c
subhalakshmi <subhalakshmi2429@gmail.com>
2025-05-11 11:01:31 +0530

    main.tf file update

--- main.tf
+++ main.tf
@@ -36,6 +36,41 @@ resource "aws_ecs_cluster" "project_cluster" {
   name = "project-cluster"
 }
 
+# Define IAM role for ECS task execution (this assumes the role doesn't exist)
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      }
+    ]
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Attach ECS task execution policy to IAM role
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
+
+# Add IAM policy for ECR repository access
+resource "aws_iam_role_policy_attachment" "ecs_task_ecr_access_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
+
 # Define the ECS task definition
 resource "aws_ecs_task_definition" "task_definition" {
   family                   = "flask-task"
@@ -66,35 +101,4 @@ resource "aws_ecs_service" "service" {
     security_groups = [var.security_group_id]
     assign_public_ip = false
   }
-}
-
-# Define IAM role for ECS task execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      }
-    ]
-  })
-}
-
-# Attach ECS task execution policy to IAM role
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_task_execution_role.name
-}
-
-# Add IAM policy for ECR repository access
-resource "aws_iam_role_policy_attachment" "ecs_task_ecr_access_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.ecs_task_execution_role.name
 }
